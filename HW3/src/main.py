5b3ba7c3ec5de896172baaa9e1cbd217c82d14dc
Yulia-Yakovleva <st041958@student.spbu.ru>
2020-12-06 13:29:28 +0300

    upd HW3

--- HW3/src/main.py
+++ HW3/src/main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import scipy.stats as stats
 import itertools
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 pth = '/home/yulia/immunogenomics/HW3/'
 
@@ -9,6 +11,7 @@ df = pd.read_csv(pth+'data/IGHV1-2_usage.tsv', sep='\t')
 haplotypes = sorted(list(set(df.Haplotype)))
 
 ### 1
+
 with open(pth+'results/table1.tsv', 'w') as out_f:
     out_f.write('Haplotype\t# individuals\tMean usage\n')
     for h in haplotypes:
@@ -16,6 +19,7 @@ with open(pth+'results/table1.tsv', 'w') as out_f:
         out_f.write(f"Haplotype {h}\t{len(subdf.Haplotype)}\t{subdf.Usage.mean()}\n")
 
 ### 2
+
 pairs = list(itertools.combinations(haplotypes, 2))
 
 anowa_rslt = pd.DataFrame()
@@ -28,3 +32,12 @@ for p1, p2 in pairs:
 anowa_rslt.fillna('-', inplace=True)
 anowa_rslt.to_csv(pth+'results/anova_rslt.tsv', sep='\t')
 
+### 3
+
+sns.set(style='darkgrid')
+sns.boxplot(data=df, x=df.Haplotype, y=df.Usage)
+plt.xticks(rotation=45)
+plt.title('Usages across haplotypes')
+plt.tight_layout()
+# plt.show()
+plt.savefig(pth+'results/boxplot.pdf', format='pdf')
